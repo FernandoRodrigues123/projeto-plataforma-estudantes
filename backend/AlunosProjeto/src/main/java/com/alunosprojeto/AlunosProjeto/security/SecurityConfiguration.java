e1d80d640044efc77672124fbf2aac41ed88dfb1
Your Name <you@example.com>
2024-02-24 16:31:03 -0300

    corrigindo permisao de teste

--- backend/AlunosProjeto/src/main/java/com/alunosprojeto/AlunosProjeto/security/SecurityConfiguration.java
+++ backend/AlunosProjeto/src/main/java/com/alunosprojeto/AlunosProjeto/security/SecurityConfiguration.java
@@ -29,9 +29,11 @@ public class SecurityConfiguration implements WebMvcConfigurer {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers( HttpMethod.POST , "/estudantes/login","/estudantes/cadastro", "/teste")
-                            .permitAll().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    req.requestMatchers( HttpMethod.POST,"/estudantes/login","/estudantes/cadastro")
+                            .permitAll().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().
+                            requestMatchers(HttpMethod.GET, "/teste").permitAll()
                     .anyRequest().authenticated();
+
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
