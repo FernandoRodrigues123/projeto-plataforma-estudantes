0f7efed8d7699b6989d6a68bcf8e07d0c32b8904
Your Name <you@example.com>
2024-02-24 16:17:36 -0300

    add teste controller

---
+++ backend/AlunosProjeto/src/main/java/com/alunosprojeto/AlunosProjeto/Api/controller/Teste.java
@@ -0,0 +1,14 @@
+package com.alunosprojeto.AlunosProjeto.Api.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/teste")
+public class Teste {
+    @GetMapping
+    public ResponseEntity<String> teste(){
+        return ResponseEntity.ok("Hello");
+    }
+}
